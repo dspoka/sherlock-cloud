73d285ee67f6f9ba4f0f1d80189c18aa7bf08c19
jirvin16 <jirvin@umail.ucsb.edu>
2015-02-20 16:52:45 -0800

    refactored wunderground code in new file sherlockRain.py

---
+++ sherlockRain.py
@@ -0,0 +1,32 @@
+import sys
+import urllib2
+import json
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def _wunderground(request):
+	f = urllib2.urlopen(request)
+	json_string = f.read()
+	parsed_json = json.loads(json_string) 
+	city_date_data = {}
+	weather_traits = ["fog", "rain", "snow", "snowfallm", "hail", "thunder", "meantempm", "meanwindspdm", "precipm"]
+	for trait in weather_traits:
+		city_date_data[trait] = parsed_json["history"]["dailysummary"][0][trait]
+	return city_date_data
+
+def _getCityData():
+	city_date_data = {}
+	cities = ["San_Francisco", "Seattle", "New_York"]
+	for city in cities:
+		city_date_data[city] = {}
+	dates = ["20060405"]
+	#for
+	request = ""
+	for city in cities:
+		for date in dates:
+			request = "http://api.wunderground.com/api/c4829adbdcc45e26/history_" + date + "/q/CA/" + city + ".json"
+			city_date_data[city][date] = _wunderground(request)
+	print city_date_data
+
+_getCityData()
